53d787b0c66e1685dec39881c378a93211c8379e
Meryem Sahin <celik.meryem1507@gmail.com>
2024-12-18 15:38:20 +0100

    Attribute und Konstruktor erstellt Gui erstellen

--- Patient.java
+++ Patient.java
@@ -1,15 +1,41 @@
 public class Patient {
+    //Attribute
     public String vorname;
     public String nachname;
     public String geburtsdatum;
     public boolean registriert;
     public String krankengeschichte;
+    public int sozialnr;
 
-    public Patient(String vorname, String nachname, String geburtsdatum, boolean registriert, String krankengeschichte) {
+    //Konstruktor
+    public Patient(String vorname, String nachname, String geburtsdatum, boolean registriert, String krankengeschichte, int sozialnr) {
         this.vorname = vorname;
         this.nachname = nachname;
         this.geburtsdatum = geburtsdatum;
         this.registriert = registriert;
         this.krankengeschichte = krankengeschichte;
+        this.sozialnr = sozialnr;
+
+
     }
+    public String getVorname() {
+        return vorname;
+    }
+    public String getNachname() {
+
+        return nachname;
+    }
+    public String getGeburtsdatum () {
+        return geburtsdatum;
+    }
+    public boolean getRegistriert () {
+        return true;
+    }
+    public String getKrankengeschichte () {
+        return krankengeschichte;
+    }
+    public int getSozialnr () {
+        return sozialnr;
+    }
+
 }
